db263e36c3fc4f25183ffaa1ef02cc09c06e0e8f
Luka Tarman <luka.tarman@gmail.com>
2022-12-23 12:40:08 +0100

    added classes

--- src/pages/SearchPage/search.page.view.js
+++ src/pages/SearchPage/search.page.view.js
@@ -1,16 +1,22 @@
 import SearchInput from "../../Components/SearchInput/search.input.view.js";
 import SettingsButton from "../../Components/SettingsButton/settings.button.view.js";
 import FavoritesButton from "../../Components/FavoritesButton/favorites.button.view.js";
+import FavoritesBar from "../../Components/FavoritesBar/favorites.bar.view.js";
 
 const SearchPage = () => {
   return (
-    <div>
+    <div className="flex">
+      <div className="w-72 xs:w-1/4 favorites-bar">
+        <FavoritesBar></FavoritesBar>
+      </div>
       <div className="top-left-favorites">
         <FavoritesButton size={6} />
       </div>
 
       <SettingsButton />
-      <SearchInput />
+      <div className="w-full">
+        <SearchInput />
+      </div>
     </div>
   );
 };
