cffae308dd01b0667f8312070eea4ab6bb28698f
Luka Tarman <luka.tarman@gmail.com>
2022-12-23 18:54:58 +0100

    added api adapter calls

--- src/adapters/http.client.adapter.js
+++ src/adapters/http.client.adapter.js
@@ -1,9 +1,24 @@
 import axios from "axios";
 import searchResults from "../assets/gradovi.json";
 
-const getSearchResults = (term) => {
-  const response = JSON.parse(searchResults);
-  return response.filter(({ city }) => {
-    city.toLowerCase().includes(term.toLowerCase());
-  });
+export const getSearchResults = (term) => {
+  const response = searchResults;
+
+  return response
+    .filter(({ city }) => city.toLowerCase().includes(term.toLowerCase()))
+    .filter((result, index) => index <= 5);
+};
+
+export const getLocationDetails = async (name) => {
+  const searchResponse = searchResults.filter(
+    ({ city }) => city.toLowerCase() === name.toLowerCase()
+  );
+  const fixedLatitude = searchResponse[0].lat.slice(0, 5);
+  const fixedLongitude = searchResponse[0].lng.slice(0, 5);
+
+  const response = await axios.get(
+    `https://api.open-meteo.com/v1/forecast?latitude=${fixedLatitude}&longitude=${fixedLongitude}`
+  );
+
+  console.log(response.data);
 };
