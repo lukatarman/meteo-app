13d928e15e3d5a0211d6cc49c4ae3556e15176aa
Luka Tarman <luka.tarman@gmail.com>
2022-12-23 18:58:47 +0100

    added code to cityDetails component

--- src/pages/CityDetails/city.details.view.js
+++ src/pages/CityDetails/city.details.view.js
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { getLocationDetails } from "../../adapters/http.client.adapter.js";
+import { selectedCityState } from "../../contexts/CityDetailsContext/index.js";
+import ViewDropdown from "../../Components/ViewDropdown/view.dropdown.view.js";
 
 const CityDetails = () => {
   let { name } = useParams();
 
-  return <div>City details #{name}</div>;
+  const selectedCity = useRecoilValue(selectedCityState);
+
+  useEffect(() => {
+    getLocationDetails(name);
+  });
+
+  return (
+    <div>
+      <div>Metorologic data for {name}</div>
+      <ViewDropdown />
+    </div>
+  );
 };
 
 export default CityDetails;
