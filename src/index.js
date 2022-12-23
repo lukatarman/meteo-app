1e56d7e912e358e7f3b8e0b9c47404f2d627be94
Luka Tarman <luka.tarman@gmail.com>
2022-12-23 18:54:36 +0100

    added recoil root

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
 import { BrowserRouter } from "react-router-dom";
 import App from "./pages/App/app.view.js";
 import "./assets/styles.css";
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>
 );
