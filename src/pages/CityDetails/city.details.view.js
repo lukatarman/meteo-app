426769de22ca38e43febf4edab3e837ee887925f
Luka Tarman <luka.tarman@gmail.com>
2022-12-25 08:57:46 +0100

    changed component name
    # added dropdown options

--- src/pages/CityDetails/city.details.view.js
+++ src/pages/CityDetails/city.details.view.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { getLocationDetails } from "../../adapters/http.client.adapter.js";
 import { selectedCityState } from "../../contexts/CityDetailsContext/index.js";
-import ViewDropdown from "../../Components/ViewDropdown/view.dropdown.view.js";
+import Dropdown from "../../Components/Dropdown/dropdown.view.js";
 
 // todo
 // Add/style JSX
@@ -19,10 +19,15 @@ const CityDetails = () => {
     getLocationDetails(name);
   });
 
+  const dropdownOptions = {
+    defaultValue: "Please Select View Type",
+    menuOptions: ["Hourly View", "Daily View"],
+  };
+
   return (
     <div>
       <div>Metorologic data for {name}</div>
-      <ViewDropdown />
+      <Dropdown options={dropdownOptions} />
     </div>
   );
 };
