9ea0f79b30744d5b8910f7aab5d1c764a4a66bf7
Luka Tarman <luka.tarman@gmail.com>
2022-12-25 10:14:04 +0100

    changed width

--- src/Components/DropdownMenu/dropdown.menu.js
+++ src/Components/DropdownMenu/dropdown.menu.js
@@ -30,7 +30,7 @@ const DropdownMenu = ({ options, setButtonValue }) => {
   return (
     <div>
       <div
-        className="z-10 mt-2 w-72 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+        className="z-10 mt-2 w-96 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
         role="menu"
         aria-orientation="vertical"
         aria-labelledby="menu-button"
