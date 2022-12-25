d4e663adf2c47f57f42fe72c618db1760e73c556
Luka Tarman <luka.tarman@gmail.com>
2022-12-25 12:51:13 +0100

    fixed dropdown bug

--- src/Components/DropdownMenu/dropdown.menu.js
+++ src/Components/DropdownMenu/dropdown.menu.js
@@ -28,7 +28,7 @@ const DropdownMenu = ({ options, setButtonValue }) => {
   });
 
   return (
-    <div>
+    <div className="absolute">
       <div
         className="z-10 mt-2 w-96 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
         role="menu"
