6906be4d05e18f65d44bca5378ba24c211568cb9
Luka Tarman <luka.tarman@gmail.com>
2022-12-26 14:16:33 +0100

    refactored weather variables component
    # added click logic

--- src/Components/WeatherVariables/weather.variables.js
+++ src/Components/WeatherVariables/weather.variables.js
@@ -1,28 +1,36 @@
-import { useRecoilState } from "recoil";
-import { weatherVariablesState } from "../../contexts/CityDetailsContext/index.js";
+import WeatherVariablesBehavior from "./weather.variables.behavior.js";
 
 const WeatherVariables = ({ options }) => {
+  const handleCheckboxClick = WeatherVariablesBehavior(options);
+
   const renderColumn = (col) => {
-    return col.map((value) => {
-      for (const property in value) {
-        return (
-          <div class="flex items-center mb-4">
-            <input
-              type="checkbox"
-              id={value[property]}
-              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 custom-checkbox"
-            />
-            <label for={value[property]} class="ml-2 text-sm font-medium text-gray-900 ">
-              {property}
-            </label>
-          </div>
-        );
-      }
+    return col.map((item, index) => {
+      return (
+        <div key={index} className="flex items-center mb-4">
+          <input
+            type="checkbox"
+            id={item.apiValue}
+            className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 custom-checkbox"
+            onClick={handleCheckboxClick}
+          />
+          <label
+            htmlFor={item.apiValue}
+            className="ml-2 text-sm font-medium text-gray-900 "
+          >
+            {item.value}
+          </label>
+        </div>
+      );
     });
   };
 
+  let uniqueKey = 0;
   const renderAllColumns = (col) => {
-    return <div className="basis-1/4">{renderColumn(col)}</div>;
+    return (
+      <div key={uniqueKey++} className="basis-1/4">
+        {renderColumn(col)}
+      </div>
+    );
   };
 
   let renderVariables = [];
