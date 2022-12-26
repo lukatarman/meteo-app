10c5004abf1c2dfabb52a17981550581b4c3766b
Luka Tarman <luka.tarman@gmail.com>
2022-12-26 21:46:54 +0100

    refactored imports
    # added todo

--- src/pages/CityDetails/city.details.view.js
+++ src/pages/CityDetails/city.details.view.js
@@ -1,12 +1,15 @@
 import Dropdown from "../../Components/Dropdown/dropdown.view.js";
 import WeatherVariables from "../../Components/WeatherVariables/weather.variables.js";
-import CityDetailsBehavior from "./city.details.behavior.js";
-import { variableTypeState } from "../../contexts/CityDetailsContext/index.js";
+import { variableTypeState } from "../../contexts/CityDetailsContext/";
 import { useRecoilValue } from "recoil";
+
+import CityDetailsBehavior from "./city.details.behavior.js";
 
 // todo
 // add api calls based on checkboxes selected
 // display data as text
+// add alert for no timezone selected on daily view
+// add settings component
 
 const CityDetails = () => {
   const variableType = useRecoilValue(variableTypeState);
