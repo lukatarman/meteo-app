e946c2927267d452165bacb8ffda1fbc0bcbb480
Luka Tarman <luka.tarman@gmail.com>
2022-12-26 21:46:24 +0100

    removed state

--- src/contexts/CityDetailsContext/index.js
+++ src/contexts/CityDetailsContext/index.js
@@ -1,10 +1,5 @@
 import { atom } from "recoil";
 
-export const selectedCityState = atom({
-  key: "CityDetails",
-  default: [],
-});
-
 export const isVisibleState = atom({
   key: "IsVisible",
   default: false,
