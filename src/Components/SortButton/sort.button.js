86964473fbd0b82719c3fdf646e63a05fc231c12
Luka Tarman <luka.tarman@gmail.com>
2022-12-28 12:43:29 +0100

    added sort button component

---
+++ src/Components/SortButton/sort.button.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { favoriteCitiesState } from "../../contexts/AppContext/index.js";
+
+const SortButton = () => {
+  const [favoriteCities, setFavoriteCities] = useRecoilState(favoriteCitiesState);
+  const [isAscending, setIsAscending] = useState("");
+
+  useEffect(() => {
+    console.log(isAscending);
+  }, [isAscending]);
+
+  const handleSortClick = () => {
+    const arrCopy = [...favoriteCities];
+
+    setIsAscending(isAscending === "" ? true : !isAscending);
+
+    const ascending = (a, b) => {
+      if (a.city < b.city) return 1;
+      if (a.city > b.city) return -1;
+    };
+    const descending = (a, b) => {
+      if (a.city < b.city) return -1;
+      if (a.city > b.city) return 1;
+    };
+
+    if (isAscending) arrCopy.sort(ascending);
+    if (!isAscending) arrCopy.sort(descending);
+
+    setFavoriteCities(arrCopy);
+  };
+
+  return (
+    <div
+      className="flex absolute top-0 left-0 m-3 cursor-pointer hover:bg-sky-200"
+      onClick={handleSortClick}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+        />
+      </svg>
+      {/* <div className="px-2">Sort</div> */}
+    </div>
+  );
+};
+
+export default SortButton;
