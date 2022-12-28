715a6d18608a0d579df520dba3609bd9db134904
Luka Tarman <luka.tarman@gmail.com>
2022-12-28 10:51:11 +0100

    added favorites bar JSX and logic

--- src/Components/FavoritesBar/favorites.bar.view.js
+++ src/Components/FavoritesBar/favorites.bar.view.js
@@ -1,5 +1,22 @@
-const FavoritesBar = () => {
-  return <div className="w-72 xs:w-1/4 favorites-bar">FAV BAR</div>;
+import { useRecoilValue } from "recoil";
+import { favoriteCitiesState } from "../../contexts/AppContext/index.js";
+import LeftArrow from "../LeftArrow/left.arrow.js";
+
+const FavoritesBar = ({ setFavoritesVisible }) => {
+  const favoriteCities = useRecoilValue(favoriteCitiesState);
+
+  const favoriteCitiesRender = favoriteCities.map((city) => {
+    return <div className="text-2xl m-2">{city}</div>;
+  });
+
+  return (
+    <div className="h-screen">
+      <div className="w-72 xs:w-1/4 favorites-bar relative h-full bg-blue-200">
+        <LeftArrow setFavoritesVisible={setFavoritesVisible} />
+        <div className="flex flex-col pt-8">{favoriteCitiesRender}</div>
+      </div>
+    </div>
+  );
 };
 
 export default FavoritesBar;
