a6431fca0124114004788732ef32a3248a8c5e07
Luka Tarman <luka.tarman@gmail.com>
2022-12-28 10:55:13 +0100

    added options, refactored JSX

--- src/pages/CityDetails/city.details.view.js
+++ src/pages/CityDetails/city.details.view.js
@@ -1,33 +1,60 @@
 import { useState } from "react";
 import Dropdown from "../../Components/Dropdown/dropdown.view.js";
-import WeatherVariables from "../../Components/WeatherVariables/weather.variables.js";
+import WeatherVariables from "../../Components/WeatherVariables/weather.variables.view.js";
 import SettingsMenu from "../../Components/SettingsMenu/settings.menu.view.js";
+import SettingsButton from "../../Components/SettingsButton/settings.button.view.js";
 import CityDetailsBehavior from "./city.details.behavior.js";
+import Button from "../../Components/Button/button.js";
+import FavoritesBar from "../../Components/FavoritesBar/favorites.bar.view.js";
+import FavoritesButton from "../../Components/FavoritesButton/favorites.button.view.js";
 
 // todo
-// add api calls based on checkboxes selected
-// display data as text
-// add alert for no timezone selected on daily view
-// add settings component
+// add graph
+// make page mobile friendly
+// animate?
 
 const CityDetails = () => {
-  const [isVisible, setIsVisible] = useState(true);
+  const [settingsIsVisible, setSettingsIsVisible] = useState(false);
 
-  const [dropdownOptions, hourlyVariables, dailyVariables, selectedCity, variableType] =
-    CityDetailsBehavior();
+  const [
+    dropdownOptions,
+    hourlyVariables,
+    dailyVariables,
+    selectedCity,
+    variableType,
+    buttonOptions,
+    favoritesVisible,
+    setFavoritesVisible,
+    favoriteButtonOptions,
+  ] = CityDetailsBehavior();
 
   return (
     <div>
-      <div className="flex flex-col items-center h-screen">
-        {isVisible ? <SettingsMenu setIsVisible={setIsVisible} /> : null}
-        <h1 className="text-4xl my-5">Metorologic data for {selectedCity.city}</h1>
-        <Dropdown options={dropdownOptions} />
-        {variableType === "Hourly View" ? (
-          <WeatherVariables options={hourlyVariables} />
+      <div className="flex">
+        {favoritesVisible ? (
+          <FavoritesBar setFavoritesVisible={setFavoritesVisible} />
         ) : null}
-        {variableType === "Daily View" ? (
-          <WeatherVariables options={dailyVariables} />
-        ) : null}
+
+        {favoritesVisible ? null : <FavoritesButton options={favoriteButtonOptions} />}
+        <div className="flex flex-col items-center mx-auto p-5 w-3/4">
+          {settingsIsVisible ? (
+            <SettingsMenu setIsVisible={setSettingsIsVisible} />
+          ) : null}
+          {settingsIsVisible ? null : (
+            <SettingsButton setSettingsIsVisible={setSettingsIsVisible} />
+          )}
+          <h1 className="text-4xl my-5">Metorologic data for {selectedCity.city}</h1>
+          <Dropdown options={dropdownOptions} />
+          {variableType === "Hourly View" ? (
+            <WeatherVariables options={hourlyVariables} />
+          ) : null}
+          {variableType === "Daily View" ? (
+            <WeatherVariables options={dailyVariables} />
+          ) : null}
+          <div className="my-5 mr-auto">
+            {variableType !== "" && <Button options={buttonOptions} />}
+          </div>
+        </div>
       </div>
     </div>
   );
