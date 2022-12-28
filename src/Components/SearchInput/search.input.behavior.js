d6a59c1bc71b1833c1b3cabe148fb4f666fec51a
Luka Tarman <luka.tarman@gmail.com>
2022-12-28 10:52:07 +0100

    used favoriteButton options variable

--- src/Components/SearchInput/search.input.behavior.js
+++ src/Components/SearchInput/search.input.behavior.js
@@ -2,10 +2,12 @@ import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { searchResultsState, searchInputState } from "../../contexts/SearchPageContenxt";
 import { getSearchResults } from "../../adapters/http.client.adapter.js";
+import { favoriteCitiesState } from "../../contexts/AppContext/index.js";
 
 const SearchInputBehavior = () => {
   const [searchResults, setSearchResults] = useRecoilState(searchResultsState);
   const [searchInput, setSearchInput] = useRecoilState(searchInputState);
+  const [favoriteCities, setFavoriteCities] = useRecoilState(favoriteCitiesState);
 
   const onInputChange = (e) => {
     setSearchInput(e.target.value);
@@ -22,7 +24,23 @@ const SearchInputBehavior = () => {
     if (searchInput !== "") fetchData();
   }, [searchInput]);
 
-  return [searchInput, onInputChange, searchResults];
+  const favoriteButtonOptions = {
+    size: 6,
+    margin: "",
+    position: ["", "top", "right"],
+    handleButtonClick() {
+      console.log("clicked");
+    },
+  };
+
+  return [
+    searchInput,
+    onInputChange,
+    searchResults,
+    favoriteButtonOptions,
+    favoriteCities,
+    setFavoriteCities,
+  ];
 };
 
 export default SearchInputBehavior;
