badb687a899102a72a994665c6011e335e94a32f
Luka Tarman <luka.tarman@gmail.com>
2022-12-28 12:42:41 +0100

    added rendering of favorite cities list

--- src/Components/FavoritesBar/favorites.bar.view.js
+++ src/Components/FavoritesBar/favorites.bar.view.js
@@ -1,23 +1,28 @@
 import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { favoriteCitiesState } from "../../contexts/AppContext/index.js";
+import SortButton from "../SortButton/sort.button.js";
 import LeftArrow from "../LeftArrow/left.arrow.js";
 
 const FavoritesBar = ({ setFavoritesVisible }) => {
   const favoriteCities = useRecoilValue(favoriteCitiesState);
 
-  const favoriteCitiesRender = favoriteCities.map((city) => {
-    console.log(city);
+  const favoriteCitiesRender = favoriteCities.map((city, index) => {
     return (
-      <Link to={`/city=${city.city}&latitude=${city.lat}&longitude=${city.lng}`}>
-        <div className="text-2xl m-2">{city.city}</div>
-      </Link>
+      <div key={index}>
+        <div>
+          <Link to={`/city=${city.city}&latitude=${city.lat}&longitude=${city.lng}`}>
+            <div className="text-2xl m-2">{city.city}</div>
+          </Link>
+        </div>
+      </div>
     );
   });
 
   return (
     <div className="h-screen">
       <div className="w-72 xs:w-1/4 favorites-bar relative h-full bg-sky-100">
+        <SortButton />
         <LeftArrow setFavoritesVisible={setFavoritesVisible} />
         <div className="flex flex-col pt-8">{favoriteCitiesRender}</div>
       </div>
