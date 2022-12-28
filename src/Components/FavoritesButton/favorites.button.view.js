99704b119f9348b8821662c5088dc33f16e14d37
Luka Tarman <luka.tarman@gmail.com>
2022-12-28 10:51:38 +0100

    added id to component

--- src/Components/FavoritesButton/favorites.button.view.js
+++ src/Components/FavoritesButton/favorites.button.view.js
@@ -1,4 +1,4 @@
-const FavoritesButton = ({ size }) => {
+const FavoritesButton = ({ options }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -6,9 +6,12 @@ const FavoritesButton = ({ size }) => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className={`w-${size} h-${size} favorites-icon m-2`}
+      id="favorites-button"
+      className={`w-${options.size} h-${options.size} favorites-icon ${options.margin} cursor-pointer ${options.position[0]} ${options.position[1]}-0 ${options.position[2]}-0`}
+      onClick={options.handleButtonClick}
     >
       <path
+        className="hover: bg-blue-100"
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
